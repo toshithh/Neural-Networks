7afee0f8b4a0c0cc28625c2fc74e58138fe4df97
Toshith <45565316+toshithh@users.noreply.github.com>
2023-12-22 22:20:11 +0530

    ADALINE Neural Network
    This is an ADALINE DNN with customizable number of layers and implementation example with AND gate is provided.
    It can be inherited to be used with any other function such as sigmoid, ReLu, etc.

--- DNN_Numpy.py
+++ DNN_Numpy.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class NN:
+class NeuralNetwork:
     def __init__(self, layers: tuple) -> None:
         self.weights = [np.ones((layers[i-1], layers[i])) for i in range(1, len(layers))]
         self.b = [np.ones(x) for x in layers[1:]]
@@ -19,21 +19,23 @@ class NN:
 
 ######## Overridable functions ##########
     def activation(self, x):
-        if x<0:
+        if x<=0:
             return 0
         else:
             return 1
 
     def gradient(self, inputs, outputs, alpha, target=None):
-        for i in range(len(outputs)):
+        for i in range(len(outputs)-1, -1, -1):
             a = inputs[i]
             while len(a)<len(self.weights[i].flatten()):
                 a = np.concatenate((a, inputs[i]))
             _1, _2 = self.weights[i].shape
             a = a.reshape(_2, _1).transpose()
             d_w = np.divide(np.multiply(target-outputs[i], a), self.weights[i])
+            #w = self.weights[i] + d_w
             self.weights[i] += alpha*d_w
             self.b[i] += target-outputs[i]
+            target = np.array(list(map(self.activation, np.dot(self.weights[i], target))))
         return self.weights
 
 #########################################
@@ -63,14 +65,14 @@ class NN:
             for i in range(len(outputs)):
                 x = inputs[i]
                 res = outputs[i]
-                nn.back_propagation(x, res, alpha)
+                self.back_propagation(x, res, alpha)
 
     def predict(self, x):
         y = []
         for i in x:
             y.append(self.feed_forward(i))
             #print(f"{i}:\n\t {y[-1]}")
-        return y
+        return np.array(y)
 
 
 
@@ -78,17 +80,18 @@ class NN:
 
 
 if __name__ == "__main__":
-    #AND Gate Implementation
+    #AND Gate implementation
+
     from sklearn.metrics import accuracy_score
     
-    nn = NN((3, 10, 4, 1))
-    input = np.array([[0, 1, 0], [0,0, 1], [1,1, 1], [1, 0, 1]])
+    nn = NeuralNetwork((3, 10, 4, 1))
+    input = np.array([[0, 1, 0], [0 ,0, 1], [1,1, 1], [1, 0, 1]])
     output = np.array([[0], [0], [1], [0]])
     
     test_x = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 0, 0], [1, 1, 0], [0, 1, 1], [1, 0, 0]])
     test_y = np.array([[0], [0], [1], [0], [0], [0], [0], [0]])
 
-    nn.train(input, output, 0.01, 1000)
+    nn.train(input, output, 0.01, 2000)
 
     pred_y = nn.predict(test_x)
 
